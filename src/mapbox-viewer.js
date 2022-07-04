ee40a0053f0bbecd2273f1a8ab9af8db01e9f94a
nicoarellano <nico14@gmail.com>
2022-07-04 09:58:09 -0400

    add fly to mapbox

--- src/mapbox-viewer.js
+++ src/mapbox-viewer.js
@@ -24,9 +24,8 @@ function initMapbox() {
   const map = new mapboxgl.Map({
     container: "mapboxContainer", // container ID
     style: mapStyles[0], // style URL
-    center: [-75.697, 45.384], // starting position [lng, lat]
-    zoom: 15, // starting zoom
-    pitch: 50,
+    center: [-98.74, 56.415], // starting position [lng, lat]
+    zoom: 4, // starting zoom
     antialias: true,
     projection: "globe", // display the map as a 3D globe
   });
@@ -52,4 +51,31 @@ function initMapbox() {
     }
     toggleMapStyle = !toggleMapStyle;
   };
+
+  // Go To Site 🏢
+  const goTo = document.getElementById("go-to");
+  let toggleGoTo = true;
+  goTo.onclick = function () {
+    if (toggleGoTo) {
+      this.setAttribute("title", "Go to Canada");
+      document.getElementById("go-to-icon").setAttribute("d", icons.worldIcon);
+      // Fly To Downsview flyTo(viewer, -79.47, 43.73, 1000, -45.0, 0);
+      // Fly to Carleton
+      flyTo(map, -75.697, 45.384);
+    } else {
+      this.setAttribute("title", "Go to site");
+      document.getElementById("go-to-icon").setAttribute("d", icons.goToIcon);
+      // Fly to Canada
+      flyTo(map, -98.74, 56.415, 4, 0);
+    }
+    toggleGoTo = !toggleGoTo;
+  };
+}
+
+function flyTo(map, lng, lat, zoom = 15, pitch = 50){map.flyTo({
+    center: [lng, lat],
+    zoom: zoom,
+    pitch: pitch,
+    duration: 2000
+    });
 }
