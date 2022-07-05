3823008f064cc386f2cab8c0866a22b3aae16a6c
nicoarellano <nico14@gmail.com>
2022-07-05 11:53:24 -0400

    mapbox ifc viewer working!

--- src/bundle.js
+++ src/bundle.js
@@ -129213,7 +129213,7 @@ mapboxgl.accessToken =
   });
 
   const modelOrigin = [-75.697, 45.384];
-  const modelAltitude = 0;
+  const modelAltitude = 10;
   const modelRotate = [Math.PI / 2, 0, 0];
    
   const modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
@@ -129254,15 +129254,80 @@ const customLayer = {
   this.scene.add(directionalLight2);
    
   // use the three.js GLTF loader to add the 3D model to the three.js scene
-  const gltfloader = new GLTFLoader();
-  gltfloader.load(
-  '../static/public-glb/CDC-MASSES.glb',
-  (gltf) => {
-  this.scene.add(gltf.scene);
+  //   const gltfloader = new GLTFLoader();
+  // gltfloader.load(
+  // '../static/public-glb/CDC-MASSES.glb',
+  // (gltf) => {
+  // this.scene.add(gltf.scene);
+  // }
+  // );
+  // this.map = map;
+   
+  // Sets up the IFC loading
+const ifcLoader = new IFCLoader();
+ifcLoader.ifcManager.setWasmPath("wasm/");
+// Load IFC file
+ifcLoader.load(
+  '../static/public_ifc/CDC-CIMS-FEDERATED_BLDGS-SUST-CIMS-DOC-MAINTENANCE_AND_GROUNDS_BLDG-AS_FOUND.ifc',
+  (ifc) => {
+  this.scene.add(ifc);
   }
   );
   this.map = map;
    
+  // for (const model of models) {
+  //   let buildingName = model.name
+  //   buildingName = buildingName.toUpperCase()
+  //   buildingName = buildingName.replace(/ /g, "_")
+  //   buildingName = buildingName.replace("BUILDING", "BLDG");
+  //   const ifcFile = `CDC-CIMS-FEDERATED_BLDGS-SUST-CIMS-DOC-${buildingName}-AS_FOUND.ifc`;
+  //   model.ifc = ifcFile;
+  // }
+  
+  // // Get the URL parameter
+  // const currentURL = window.location.href;
+  // const url = new URL(currentURL);
+  // const currentModelID = url.searchParams.get("id");
+  
+  // // Get the current model
+  // const currentModel = models.find(
+  //   (model) => model.code == currentModelID
+  // );
+  
+  // const pageTitle = document.getElementById("model-title");
+  
+  // if (currentModel !== undefined) {
+  //   pageTitle.innerHTML = currentModel.name
+  //   const ifcFile = `../static/public_ifc/${currentModel.ifc}`;
+  // ifcLoader.load(ifcFile, (ifcModel) => {
+  //   this.scene.add(ifcModel);
+  // });}
+  //   else{
+  //     pageTitle.innerHTML = "IFC Model"
+  //   }
+  
+  // // Load IFC file
+  // const input = document.getElementById("file-input");
+  //   input.addEventListener(
+  //     "change",
+  //     (changed) => {
+  //       const file = changed.target.files[0];
+  //       var ifcURL = URL.createObjectURL(file);
+  //       ifcLoader.load(
+  //             ifcURL,
+  //             (ifcModel) => scene.add(ifcModel));
+  //     },
+  //     false
+  //   );
+  
+
+
+
+
+
+
+
+
   // use the Mapbox GL JS map canvas for three.js
   this.renderer = new WebGLRenderer({
   canvas: map.getCanvas(),
@@ -129314,129 +129379,3 @@ const customLayer = {
   map.on('style.load', () => {
   map.addLayer(customLayer, 'waterway-label');
   });
-
-
-
-
-//   //Creates the Three.js scene
-// const scene = new Scene();
-
-// //Object to store the size of the viewport
-// const size = {
-//   width: window.innerWidth,
-//   height: window.innerHeight,
-// };
-
-// //Creates the camera (point of view of the user)
-// const aspect = size.width / size.height;
-// const camera = new PerspectiveCamera(40, aspect,1,100000);
-// camera.position.x = -350;
-// camera.position.y = 280;
-// camera.position.z = 350;
-
-// const cameraButton = document.getElementById("camera");
-// cameraToggle = true;
-// cameraButton.addEventListener("click", () => {
-//   if (cameraToggle) {
-//   console.log("WIP Ortho camera")
-// }
-// else{
-//   console.log("Perspective camera")
-// }
-// cameraToggle = !cameraToggle
-// });
-
-// //Creates the lights of the scene
-// const lightColor = 0xffffff;
-
-// const ambientLight = new AmbientLight(lightColor, 0.5);
-// scene.add(ambientLight);
-
-// //Sets up the renderer, fetching the canvas of the HTML
-// const threeCanvas = document.getElementById("three-canvas");
-// const renderer = new WebGLRenderer({
-//   canvas: threeCanvas,
-//   alpha: true,
-// });
-
-// renderer.setSize(size.width, size.height);
-// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-// //Creates grids and axes in the scene
-// const axes = new AxesHelper();
-// axes.material.depthTest = false;
-// axes.renderOrder = 1;
-// scene.add(axes);
-
-// //Creates the orbit controls (to navigate the scene)
-// const controls = new OrbitControls(camera, threeCanvas);
-// controls.enableDamping = true;
-// controls.target.set(-2, 0, 0);
-
-// //Animation loop
-// const animate = () => {
-//   controls.update();
-//   renderer.render(scene, camera);
-//   requestAnimationFrame(animate);
-// };
-
-// animate();
-
-// //Adjust the viewport to the size of the browser
-// window.addEventListener("resize", () => {
-//   size.width = window.innerWidth;
-//   size.height = window.innerHeight;
-//   camera.aspect = size.width / size.height;
-//   camera.updateProjectionMatrix();
-//   renderer.setSize(size.width, size.height);
-// });
-
-// Sets up the IFC loading
-const ifcLoader = new IFCLoader();
-ifcLoader.ifcManager.setWasmPath("wasm/");
-
-
-// for (const model of models) {
-//   let buildingName = model.name
-//   buildingName = buildingName.toUpperCase()
-//   buildingName = buildingName.replace(/ /g, "_")
-//   buildingName = buildingName.replace("BUILDING", "BLDG");
-//   const ifcFile = `CDC-CIMS-FEDERATED_BLDGS-SUST-CIMS-DOC-${buildingName}-AS_FOUND.ifc`;
-//   model.ifc = ifcFile;
-// }
-
-// // Get the URL parameter
-// const currentURL = window.location.href;
-// const url = new URL(currentURL);
-// const currentModelID = url.searchParams.get("id");
-
-// // Get the current model
-// const currentModel = models.find(
-//   (model) => model.code == currentModelID
-// );
-
-// const pageTitle = document.getElementById("model-title");
-
-if (currentModel !== undefined) {
-  pageTitle.innerHTML = currentModel.name;
-  const ifcFile = `../static/public_ifc/${currentModel.ifc}`;
-ifcLoader.load(ifcFile, (ifcModel) => {
-  scene.add(ifcModel);
-});}
-  else {
-    pageTitle.innerHTML = "IFC Model";
-  }
-
-// // Load IFC file
-// const input = document.getElementById("file-input");
-//   input.addEventListener(
-//     "change",
-//     (changed) => {
-//       const file = changed.target.files[0];
-//       var ifcURL = URL.createObjectURL(file);
-//       ifcLoader.load(
-//             ifcURL,
-//             (ifcModel) => scene.add(ifcModel));
-//     },
-//     false
-//   );
