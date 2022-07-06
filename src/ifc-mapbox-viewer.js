be4f2ed24d7eaee269c368d1a23555793cd422b8
nicoarellano <nico14@gmail.com>
2022-07-06 08:40:57 -0400

    adding osm buildings

--- src/ifc-mapbox-viewer.js
+++ src/ifc-mapbox-viewer.js
@@ -202,6 +202,56 @@ const modelTransform = {
   scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits(),
 };
 
+map.on('load', () => {
+  // Insert the layer beneath any symbol layer.
+  const layers = map.getStyle().layers;
+  const labelLayerId = layers.find(
+  (layer) => layer.type === 'symbol' && layer.layout['text-field']
+  ).id;
+   
+  // The 'building' layer in the Mapbox Streets
+  // vector tileset contains building height data
+  // from OpenStreetMap.
+  map.addLayer(
+  {
+  'id': 'add-3d-buildings',
+  'source': 'composite',
+  'source-layer': 'building',
+  'filter': ['==', 'extrude', 'true'],
+  'type': 'fill-extrusion',
+  'minzoom': 15,
+  'paint': {
+  'fill-extrusion-color': '#aaa',
+   
+  // Use an 'interpolate' expression to
+  // add a smooth transition effect to
+  // the buildings as the user zooms in.
+  'fill-extrusion-height': [
+  'interpolate',
+  ['linear'],
+  ['zoom'],
+  15,
+  0,
+  15.05,
+  ['get', 'height']
+  ],
+  'fill-extrusion-base': [
+  'interpolate',
+  ['linear'],
+  ['zoom'],
+  15,
+  0,
+  15.05,
+  ['get', 'min_height']
+  ],
+  'fill-extrusion-opacity': 0.9
+  }
+  },
+  labelLayerId
+  );
+  });
+
+// THREE JS 3️⃣
 const THREE = window.THREE;
 // configuration of the custom layer for a 3D model per the CustomLayerInterface
 const customLayer = {
