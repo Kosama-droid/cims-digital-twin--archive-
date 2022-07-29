3093124dff05b988c77bf84ead93411603de09d2
nicoarellano <nico14@gmail.com>
2022-07-29 16:07:52 -0400

    params

--- rollup.config.js
+++ rollup.config.js
@@ -13,16 +13,16 @@ export default [
     resolve(),
   ],
 },
-// {
-//   input: "src/bim-viewer-wiv.js",
-//   output: [
-//     {
-//     format: "esm",
-//     file: "./dist/bim-viewer-wiv-bundle.js"
-//     }
-//   ],
-//   plugins: [
-//     resolve(),
-//   ],
-// },
+{
+  input: "src/bim-viewer-wiv.js",
+  output: [
+    {
+    format: "esm",
+    file: "./dist/bim-viewer-wiv-bundle.js"
+    }
+  ],
+  plugins: [
+    resolve(),
+  ],
+},
 ]
