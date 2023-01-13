a6e69093cf9aa74c9f7c0189e470632d696b4479
Ko <55330447+Kosama-droid@users.noreply.github.com>
2023-01-13 13:21:19 -0500

    Kolene (#21)
    * test message in branck kolene
    
    * sending a test txt file in datasets (#15)
    
    * modification to readme (#13)
    
    * test update from KP
    
    Co-authored-by: Nico Arellano <95719187+nicoarellano@users.noreply.github.com>
    Co-authored-by: Kenneth Percy <kpercy@cimslab.ca>
    
    * trying to set clone branch's upstream to kolene branch
    
    * changed test file
    
    Co-authored-by: ken-99 <101900365+ken-99@users.noreply.github.com>
    Co-authored-by: Nico Arellano <95719187+nicoarellano@users.noreply.github.com>
    Co-authored-by: Kenneth Percy <kpercy@cimslab.ca>

--- test/placeModel.js
+++ test/placeModel.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 // We create a schema for place
 let placeSchema = Schema({
     name: { type: String, required: true },
-    id: { type: String, required: true },
-    geoJson: {type : Array, required: true},
-    city: {type: String, required: true}
+    id: { type: String, required: false },
+    placeGeoJson: {type : Array, required: false},
+    city: {type: String, required: false}
 });
 
-const place = mongoose.model("place", placeSchema);
-module.exports = place;
+const Place = mongoose.model("place", placeSchema);
+export default Place;
