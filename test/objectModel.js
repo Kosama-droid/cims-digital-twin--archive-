145b8c04f3bff3016025e3f4484e215d732b45d8
Ko <lamizanakolene@yahoo.fr>
2023-01-13 13:03:04 -0500

    changed test file

--- test/objectModel.js
+++ test/objectModel.js
@@ -1,17 +1,20 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+//const { DefaultLoadingManager } = require("three");
 const Schema = mongoose.Schema;
 
 //using schema 
-const place = require("./placeModel.js");
+import place from "./placeModel.js";
 
-// We create a schema for place
+// We create a schema for Objects
 let objectSchema = Schema({
     name: { type: String, required: true },
-    id: { type: String, required: true },
-    geoJson: {type : Array, required: true},
-    city: {type: String, required: true},
-    place: {type: place, required: false} //maybe just the name of place?
+    //id: { type: String, required: true },
+    ifcFileName: {type : String, required: false},
+    city: {type: String, required: false},
+    place: {type: String, required: false}
+    //place: {type: place, required: false} //maybe just the name of place?
+    //owner: {type: owner, required: false}
 });
 
-const object = mongoose.model("object", objectSchema);
-module.exports = object;
+const Object = mongoose.model("Object", objectSchema);
+export default Object;
