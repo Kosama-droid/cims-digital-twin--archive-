f45871540fde9d31f0642749dac9a7634470d2e5
Nico Arellano <95719187+nicoarellano@users.noreply.github.com>
2023-01-16 13:29:29 -0500

    Nico wip1 (#28)
    * created 2 new html files with different api fetch types and responses (#24)
    
    * modification to readme (#13)
    
    * test update from KP
    
    * This is a basic API to fetch bicycle parking in TO
    
    * delete test txt file
    
    * Delete COMMIT-TEST-1.txt
    
    * [1 version of fetch retrieving the database and the data, the second with jquery retrieving metadata about the database and showing a popup with the info]
    
    Co-authored-by: Nico Arellano <95719187+nicoarellano@users.noreply.github.com>
    Co-authored-by: Kenneth Percy <kpercy@cimslab.ca>
    Co-authored-by: Ken Percy <kpercy@cims.carleton.ca>
    
    * working on api fetch
    
    Co-authored-by: ken-99 <101900365+ken-99@users.noreply.github.com>
    Co-authored-by: Kenneth Percy <kpercy@cimslab.ca>
    Co-authored-by: Ken Percy <kpercy@cims.carleton.ca>

--- datasets/openToronto.js
+++ datasets/openToronto.js
@@ -1,9 +1,10 @@
 export default function openTorontoTest() {
+  // promise to retrieve the package
+
   const https = require("https"),
     packageId = "a3309088-5fd4-4d34-8297-77c8301840ac";
 
   // promise to retrieve the package
-
   const getPackage = new Promise((resolve, reject) => {
     https.get(
       `https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/package_show?id=${packageId}`,
@@ -16,69 +17,64 @@ export default function openTorontoTest() {
           .on("end", () => {
             let data = Buffer.concat(dataChunks);
             resolve(JSON.parse(data.toString())["result"]);
-            console.log(data, "19");
           })
           .on("error", (error) => {
             reject(error);
           });
-
-        console.log(response, "25");
       }
     );
   });
+
+  getPackage
+    .then((pkg) => {
+      // this is the metadata of the package
+      //console.log(pkg);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  // since this package has resources in the datastore, one can get the data rather than just the metadata of the resources
+  // promise to retrieve data of a datastore resource
+  const getDatastoreResource = (resource) =>
+    new Promise((resolve, reject) => {
+      https.get(
+        `https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/datastore_search?id=${resource["id"]}`,
+        (response) => {
+          let dataChunks = [];
+          response
+            .on("data", (chunk) => {
+              dataChunks.push(chunk);
+            })
+            .on("end", () => {
+              let data = Buffer.concat(dataChunks);
+              resolve(JSON.parse(data.toString())["result"]["records"]);
+            })
+            .on("error", (error) => {
+              reject(error);
+            });
+        }
+      );
+    });
+
+  // get the package information again
+  getPackage
+    .then((phg) => {
+      // get the datastore resources for the phg
+      let datastoreResources = phg["resources"].filter(
+        (r) => r.datastore_active
+      );
+
+      // retrieve the first datastore resource as an example
+      getDatastoreResource(datastoreResources[0])
+        .then((resource) => {
+          // this is the actual data of the resource
+          //console.log(resource)
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
-
-/*
-getPackage
-  .then((pkg) => {
-    // this is the metadata of the package
-    console.log(pkg);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-// since this package has resources in the datastore, one can get the data rather than just the metadata of the resources
-// promise to retrieve data of a datastore resource
-const getDatastoreResource = (resource) =>
-  new Promise((resolve, reject) => {
-    https.get(
-      `https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/datastore_search?id=${resource["id"]}`,
-      (response) => {
-        let dataChunks = [];
-        response
-          .on("data", (chunk) => {
-            dataChunks.push(chunk);
-          })
-          .on("end", () => {
-            let data = Buffer.concat(dataChunks);
-            resolve(JSON.parse(data.toString())["result"]["records"]);
-          })
-          .on("error", (error) => {
-            reject(error);
-          });
-      }
-    );
-  });
-
-// get the package information again
-getPackage
-  .then((package) => {
-    // get the datastore resources for the package
-    let datastoreResources = package["resources"].filter(
-      (r) => r.datastore_active
-    );
-
-    // retrieve the first datastore resource as an example
-    getDatastoreResource(datastoreResources[0])
-      .then((resource) => {
-        // this is the actual data of the resource
-        console.log(resource);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-*/
